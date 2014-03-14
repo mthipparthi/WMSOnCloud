016d95616bb95cea1b9f877ec2fcd2e8eae12315
Mahesh <pjmkit@gmail.com>
2014-03-14 03:46:38 +0000

    Added Store Master App

---
+++ webapp/storemaster/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from storemaster.models import StoreMaster
+
+class StoreMasterForm(forms.ModelForm):
+	#store_id = forms.CharField()
+	#store_name = forms.CharField(widget=forms.Textarea)
+	class Meta:
+		model = StoreMaster
+		fields = ['store_id','store_name','oraganisation_id','address_1','address_2','suburb','state','country','post_code','phone_nbr','dsitribution_centre_flag','rcv_without_shipment','locations_exist','local_currency','display_local_currency','create_user_id','create_date_time','receive_with_lpn','putaway_tasks_needed','picking_tasks_enabled','store_order_allocation_enabled','cash_sale_return_period','credit_sale_return_period'] # list of fields you want from model
+
---
+++ webapp/storemaster/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from datetime import datetime 
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+class StoreMaster(models.Model):
+	store_id = models.CharField(max_length=12)
+	store_name = models.CharField(max_length=70)
+	oraganisation_id = models.CharField(max_length=3)
+	address_1 = models.CharField(max_length=35)
+	address_2 = models.CharField(max_length=35)
+	suburb = models.CharField(max_length=35)
+	state = models.CharField(max_length=35)
+	country = models.CharField(max_length=10)
+	post_code = models.CharField(max_length=10)
+	phone_nbr = models.CharField(max_length=20)
+	dsitribution_centre_flag = models.BooleanField(default=True)
+	rcv_without_shipment = models.BooleanField(default=True)
+	locations_exist = models.BooleanField(default=True)
+	local_currency = models.CharField(max_length=3)
+	display_local_currency = models.BooleanField(default=True)
+	create_user_id = models.CharField(max_length=10)
+	create_date_time = models.DateTimeField(default=datetime.now, blank=True)
+	receive_with_lpn = models.BooleanField(default=True)
+	putaway_tasks_needed = models.BooleanField(default=True)
+	picking_tasks_enabled = models.BooleanField(default=True)
+	store_order_allocation_enabled = models.BooleanField(default=True)
+	cash_sale_return_period = models.BooleanField(default=True)
+	credit_sale_return_period = models.BooleanField(default=True)
+	
+	def get_absolute_url(self):
+		return reverse('storemaster-detail', kwargs={'pk': self.pk})
